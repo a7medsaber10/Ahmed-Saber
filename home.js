0468704f40dace25482853b43363c6b9bf727933
Ahmed Saber <100302217+a7medsaber10@users.noreply.github.com>
2022-05-18 20:58:52 +0200

    Add files via upload

--- home.js
+++ home.js
@@ -2,12 +2,23 @@
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
 
-btn.onclick = function () {
-  sidebar.classList.toggle("active");
-};
+// btn.onclick = function () {
+//   sidebar.classList.toggle("active");
+// };
 // --------------------------------
 // scroll to top
 function scrollToTop() {
   window.scrollTo(0, 0);
 }
 // --------------------------------
+let section = document.querySelector(".contain");
+let bars = document.querySelectorAll(".bars");
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop - 300) {
+    console.log("reached section");
+    bars.forEach((bar) => {
+      bar.style.width = bar.dataset.width;
+    });
+  }
+};
